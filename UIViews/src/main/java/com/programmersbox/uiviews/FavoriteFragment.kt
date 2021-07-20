004b27ea962481752970814d2e781c1232b61be5
jakepurple13 <jakerein@gmail.com>
2021-07-20 14:52:59 -0400

    - started jetpack compose integration

--- UIViews/src/main/java/com/programmersbox/uiviews/FavoriteFragment.kt
+++ UIViews/src/main/java/com/programmersbox/uiviews/FavoriteFragment.kt
@@ -5,13 +5,35 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.view.children
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.accompanist.glide.rememberGlidePainter
+import com.google.accompanist.imageloading.ImageLoadState
 import com.google.android.material.chip.Chip
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.jakewharton.rxbinding2.widget.textChanges
@@ -60,6 +82,8 @@ class FavoriteFragment : BaseFragment() {
 
     private lateinit var binding: FragmentFavoriteBinding
 
+    @ExperimentalMaterialApi
+    @ExperimentalFoundationApi
     @SuppressLint("SetTextI18n")
     override fun viewCreated(view: View, savedInstanceState: Bundle?) {
 
@@ -101,6 +125,45 @@ class FavoriteFragment : BaseFragment() {
                 else binding.emptyState.visible()
             }
             .addTo(disposable)
+
+        binding.xmlVersion.visibility = View.GONE
+
+        binding.composeVersion.setContent {
+
+            val list = remember { mutableStateMapOf<String, List<DbModel>>() }
+
+            val data = remember {
+                Flowables.combineLatest(
+                    source1 = dbFire
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread()),
+                    source2 = sourcePublisher.toLatestFlowable(),
+                    source3 = binding.favSearchInfo
+                        .textChanges()
+                        .debounce(500, TimeUnit.MILLISECONDS)
+                        .toLatestFlowable()
+                )
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .map { pair ->
+                        pair.first.sortedBy(DbModel::title)
+                            .filter { it.source in pair.second.map(ApiService::serviceName) && it.title.contains(pair.third, true) }
+                    }
+                    .map { it.size to it.toGroup() }
+                    .distinctUntilChanged()
+                    .subscribe {
+                        binding.favSearchInfo.setAdapter(ArrayAdapter(requireContext(), R.layout.favorite_auto_item, it.second.map { it.key }))
+                        list.clear()
+                        list.putAll(it.second)
+                    }
+                    .addTo(disposable)
+            }
+
+            LazyVerticalGrid(cells = GridCells.Adaptive(180.dp)) {
+                items(items = list.values.sortedBy { it.first().title }.toTypedArray()) { FavoriteItem(it, findNavController()) }
+            }
+
+        }
 
         binding.viewRecentList.setOnClickListener { (activity as? BaseMainActivity)?.goToScreen(BaseMainActivity.Screen.RECENT) }
 
@@ -215,6 +278,88 @@ class FavoriteFragment : BaseFragment() {
             binding.executePendingBindings()
         }
 
+    }
+
+    private val logo: MainLogo by inject()
+
+    @ExperimentalMaterialApi
+    @Composable
+    private fun FavoriteItem(info: List<DbModel>, navController: NavController) {
+        Card(
+            onClick = {
+                val item = info.firstOrNull()?.let { genericInfo.toSource(it.source)?.let { it1 -> it.toItemModel(it1) } }
+                navController.navigate(FavoriteFragmentDirections.actionFavoriteFragmentToDetailsFragment(item))
+            },
+            modifier = Modifier
+                .padding(5.dp)
+                .size(
+                    with(LocalDensity.current) { 360.toDp() },
+                    with(LocalDensity.current) { 480.toDp() }
+                ),
+            interactionSource = MutableInteractionSource()
+        ) {
+
+            val item = info.firstOrNull()
+
+            val painter = rememberGlidePainter(
+                item?.imageUrl,
+                previewPlaceholder = logo.logoId
+            )
+
+            Box {
+                Image(
+                    painter = painter,
+                    contentDescription = "",
+                    //contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .border(BorderStroke(1.dp, Color(0x00000000)), shape = RoundedCornerShape(5.dp))
+                        .align(Alignment.Center)
+                )
+
+                when (painter.loadState) {
+                    is ImageLoadState.Loading -> {
+                        // Display a circular progress indicator whilst loading
+                        CircularProgressIndicator(Modifier.align(Alignment.Center))
+                    }
+                    is ImageLoadState.Error -> {
+                        // If you wish to display some content if the request fails
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ),
+                                startY = 50f
+                            )
+                        )
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    Text(
+                        item?.title.orEmpty(),
+                        style = MaterialTheme
+                            .typography
+                            .body1
+                            .copy(textAlign = TextAlign.Center, color = Color.White),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
+                    )
+                }
+            }
+
+        }
     }
 
     companion object {
