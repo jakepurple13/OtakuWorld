41793fdb9d27951d360497b87f9150abe93237b1
jakepurple13 <jakerein@gmail.com>
2021-09-29 15:39:41 -0400

    - added a MaterialCard that's based off the card from https://material.io/components/cards#anatomy

--- UIViews/src/main/java/com/programmersbox/uiviews/utils/ComposableUtils.kt
+++ UIViews/src/main/java/com/programmersbox/uiviews/utils/ComposableUtils.kt
@@ -1078,3 +1078,44 @@ fun <T> GroupButton(
         }
     }
 }
+
+@ExperimentalMaterialApi
+@Composable
+fun MaterialCard(
+    modifier: Modifier = Modifier,
+    headerOnTop: Boolean = true,
+    shape: Shape = MaterialTheme.shapes.medium,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    border: BorderStroke? = null,
+    elevation: Dp = 1.dp,
+    header: (@Composable ColumnScope.() -> Unit)? = null,
+    media: (@Composable ColumnScope.() -> Unit)? = null,
+    supportingText: (@Composable () -> Unit)? = null,
+    actions: (@Composable RowScope.() -> Unit)? = null
+) {
+    Card(
+        modifier = modifier,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        border = border,
+        elevation = elevation
+    ) {
+        Column {
+            if (headerOnTop) header?.invoke(this)
+            media?.invoke(this)
+            if (!headerOnTop) header?.invoke(this)
+            Box(modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)) {
+                supportingText?.let {
+                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                        ProvideTextStyle(MaterialTheme.typography.body2) {
+                            it.invoke()
+                        }
+                    }
+                }
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) { actions?.invoke(this) }
+        }
+    }
+}
--- app/src/main/java/com/programmersbox/otakuworld/MainActivity.kt
+++ app/src/main/java/com/programmersbox/otakuworld/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +26,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastMap
@@ -34,6 +39,7 @@ import com.programmersbox.models.ApiService
 import com.programmersbox.models.ItemModel
 import com.programmersbox.models.SwatchInfo
 import com.programmersbox.uiviews.utils.ComposableUtils
+import com.programmersbox.uiviews.utils.MaterialCard
 import com.programmersbox.uiviews.utils.toComposeColor
 import com.skydoves.landscapist.glide.GlideImage
 import com.skydoves.landscapist.palette.BitmapPalette
@@ -43,6 +49,7 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import kotlin.random.Random
 import com.programmersbox.anime_sources.Sources as ASources
 import com.programmersbox.manga_sources.Sources as MSources
 import com.programmersbox.novel_sources.Sources as NSources
@@ -67,7 +74,7 @@ class MainActivity : ComponentActivity() {
                     it
                         .getRecent()
                         .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
+                        .observeOn(Schedulers.io())
                 }
         )
             .subscribeOn(Schedulers.io())
@@ -113,11 +120,20 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.spacedBy(4.dp)
                     ) {
                         items(sourceList) {
+                            InfoCard2(info = it, favorites = favorites) {
+                                val db = it.toDbModel()
+                                if (db in favorites) favorites.remove(db) else favorites.add(db)
+                            }
+                        }
+
+                        items(sourceList) {
                             InfoCard(info = it, favorites = favorites) {
                                 val db = it.toDbModel()
                                 if (db in favorites) favorites.remove(db) else favorites.add(db)
                             }
                         }
+
+                        item { MaterialCardPreview() }
                     }
                 }
 
@@ -145,6 +161,81 @@ private fun Color.animate() = animateColorAsState(this)
 
 @ExperimentalMaterialApi
 @Composable
+fun InfoCard2(
+    info: ItemModel,
+    favorites: List<DbModel>,
+    onClick: () -> Unit
+) {
+    val swatchInfo = remember { mutableStateOf<SwatchInfo?>(null) }
+
+    MaterialCard(
+        backgroundColor = swatchInfo.value?.rgb?.toComposeColor()?.animate()?.value ?: MaterialTheme.colors.surface,
+        modifier = Modifier.clickable { onClick() },
+        media = {
+            GlideImage(
+                imageModel = info.imageUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                requestBuilder = Glide.with(LocalContext.current.applicationContext).asDrawable(),
+                bitmapPalette = BitmapPalette { p ->
+                    swatchInfo.value = p.vibrantSwatch?.let { s -> SwatchInfo(s.rgb, s.titleTextColor, s.bodyTextColor) }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(ComposableUtils.IMAGE_HEIGHT)
+                    /*.size(
+                        ComposableUtils.IMAGE_WIDTH,
+                        ComposableUtils.IMAGE_HEIGHT
+                    )*/
+                    .align(Alignment.CenterHorizontally)
+            )
+        },
+        headerOnTop = false,
+        header = {
+            ListItem(
+                icon = {
+                    Box {
+                        if (favorites.fastAny { f -> f.url == info.url }) {
+                            Icon(
+                                Icons.Default.Favorite,
+                                contentDescription = null,
+                                tint = swatchInfo.value?.bodyColor?.toComposeColor()?.animate()?.value ?: MaterialTheme.colors.primary,
+                                modifier = Modifier.align(Alignment.TopStart)
+                            )
+                        }
+                        Icon(
+                            Icons.Default.FavoriteBorder,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onPrimary,
+                            modifier = Modifier.align(Alignment.TopStart)
+                        )
+                    }
+                },
+                text = {
+                    Text(
+                        info.title,
+                        color = swatchInfo.value?.bodyColor?.toComposeColor()?.animate()?.value ?: Color.Unspecified
+                    )
+                },
+                secondaryText = {
+                    Text(
+                        info.source.serviceName,
+                        color = swatchInfo.value?.titleColor?.toComposeColor()?.animate()?.value ?: Color.Unspecified
+                    )
+                }
+            )
+        },
+        supportingText = {
+            Text(
+                info.description,
+                color = swatchInfo.value?.titleColor?.toComposeColor()?.animate()?.value ?: Color.Unspecified
+            )
+        }
+    )
+}
+
+@ExperimentalMaterialApi
+@Composable
 fun InfoCard(
     info: ItemModel,
     favorites: List<DbModel>,
@@ -159,9 +250,7 @@ fun InfoCard(
         onClick = onClick
     ) {
         Column {
-
             Row {
-
                 GlideImage(
                     imageModel = info.imageUrl,
                     contentDescription = null,
@@ -234,5 +323,178 @@ fun InfoCard(
 
             }
         }
+    }
+}
+
+fun Random.nextColor(
+    alpha: Int = nextInt(0, 255),
+    red: Int = nextInt(0, 255),
+    green: Int = nextInt(0, 255),
+    blue: Int = nextInt(0, 255)
+) = Color(red, green, blue, alpha)
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun MaterialCardPreview() {
+
+    val media: @Composable ColumnScope.() -> Unit = {
+        Image(
+            painter = painterResource(id = R.drawable.github_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(ComposableUtils.IMAGE_WIDTH)
+                .align(Alignment.CenterHorizontally)
+                .background(Color.DarkGray)
+        )
+    }
+
+    val actions: @Composable RowScope.() -> Unit = {
+        TextButton(
+            onClick = {},
+            modifier = Modifier.weight(1f)
+        ) { Text("Action 1", style = MaterialTheme.typography.button) }
+        TextButton(
+            onClick = {},
+            modifier = Modifier.weight(1f)
+        ) { Text("Action 2", style = MaterialTheme.typography.button) }
+
+        Icon(
+            Icons.Default.Favorite,
+            null,
+            modifier = Modifier.weight(1f)
+        )
+        Icon(
+            Icons.Default.Share,
+            null,
+            modifier = Modifier.weight(1f)
+        )
+    }
+
+    val supportingText: @Composable () -> Unit = {
+        Text("Greyhound divisively hello coldly wonderfully marginally far upon excluding.")
+    }
+
+    Column(
+        modifier = Modifier.padding(5.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        MaterialCard(
+            supportingText = supportingText,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") },
+                    icon = { Icon(Icons.Default.Image, null) }
+                )
+            },
+            media = media,
+            actions = actions
+        )
+
+        MaterialCard(
+            headerOnTop = false,
+            supportingText = supportingText,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            },
+            media = media,
+            actions = actions
+        )
+
+        MaterialCard(
+            headerOnTop = false,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            },
+            media = media,
+            actions = actions
+        )
+
+        MaterialCard(
+            supportingText = supportingText,
+            media = media,
+            actions = actions
+        )
+
+        MaterialCard(
+            headerOnTop = false,
+            supportingText = supportingText,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            },
+            actions = actions
+        )
+
+        MaterialCard(
+            headerOnTop = false,
+            supportingText = supportingText,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            },
+            media = media,
+        )
+
+        MaterialCard(
+            media = media,
+            actions = actions
+        )
+
+        MaterialCard(
+            headerOnTop = false,
+            supportingText = supportingText,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            }
+        )
+
+        MaterialCard(
+            supportingText = supportingText,
+            actions = actions
+        )
+
+        MaterialCard(
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            },
+            actions = actions
+        )
+
+        MaterialCard(
+            shape = RoundedCornerShape(16.dp),
+            backgroundColor = Color.Blue,
+            border = BorderStroke(1.dp, Color.Red),
+            elevation = 5.dp,
+            headerOnTop = false,
+            supportingText = supportingText,
+            header = {
+                ListItem(
+                    text = { Text("Title goes here") },
+                    secondaryText = { Text("Secondary text") }
+                )
+            },
+            media = media,
+            actions = actions
+        )
+
     }
 }
