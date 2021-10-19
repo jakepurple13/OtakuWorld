37b05a23d19bf82aec0d2c9f73622b3f18c3d55e
jakepurple13 <jakerein@gmail.com>
2021-10-19 09:22:18 -0400

    - added kts in. Still need to explore and the pure reason why I'm not doing a full convert to kits is because dependabot has issues with it.

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,7 @@
+object Deps {
+    const val otakuVersionName = "28.9.0"
+    const val compileVersion = 31
+    const val minimumSdk = 23
+    const val targetSdk = 31
+    const val buildVersion = "30.0.2"
+}
