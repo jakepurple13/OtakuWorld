b260c84de2696f8c2d35faceb4f2c251fbd15513
jakepurple13 <jakerein@gmail.com>
2021-10-19 10:52:38 -0400

    - all variables that either won't change or are pure manual changes will be in the Dependencies.kt file

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,4 +4,19 @@ object Deps {
     const val minimumSdk = 23
     const val targetSdk = 31
     const val buildVersion = "30.0.2"
+
+    private const val jakepurple13 = "10.6.5"
+
+    const val flowutils = "com.github.jakepurple13.HelpfulTools:flowutils:$jakepurple13"
+    const val gsonutils = "com.github.jakepurple13.HelpfulTools:gsonutils:$jakepurple13"
+    const val helpfulutils = "com.github.jakepurple13.HelpfulTools:helpfulutils:$jakepurple13"
+    const val loggingutils = "com.github.jakepurple13.HelpfulTools:loggingutils:$jakepurple13"
+    const val dragswipe = "com.github.jakepurple13.HelpfulTools:dragswipe:$jakepurple13"
+    const val funutils = "com.github.jakepurple13.HelpfulTools:funutils:$jakepurple13"
+    const val rxutils = "com.github.jakepurple13.HelpfulTools:rxutils:$jakepurple13"
+    const val thirdpartyutils = "com.github.jakepurple13.HelpfulTools:thirdpartyutils:$jakepurple13"
+
+    /*const val jakepurple13Tools = arrayOf(
+        gsonutils, helpfulutils, loggingutils, funutils, rxutils, dragswipe
+    )*/
 }
