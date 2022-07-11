e9493c2d29eab5d7a6ffdb0f1fe9396db22645f5
Jacob Rein <jrein@phunware.com>
2022-07-11 12:35:18 -0400

    Version update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object Deps {
-    const val otakuVersionName = "30.0.0"
+    const val otakuVersionName = "30.0.1"
     const val compileVersion = 32
     const val minimumSdk = 23
     const val targetSdk = 32
