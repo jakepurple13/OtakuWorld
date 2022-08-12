85cd8529fe90e8c3e3aad9cc828fb663e47f034f
Jacob Rein <>
2022-08-12 11:24:07 -0400

    Making sure the classpaths will match versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
         classpath("com.google.gms:google-services:4.3.13")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
         classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:10.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${libs.versions.kotlin.get()}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${libs.versions.navVersion.get()}")
     }
 }
 
