333c4eac55aed4b0db11769df8a510742beed0c7
Jacob Rein <>
2022-08-15 09:58:54 -0400

    Updating libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
         classpath("com.google.gms:google-services:4.3.13")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
-        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:10.4.0")
+        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:${libs.versions.latestAboutLibsRelease.get()}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${libs.versions.kotlin.get()}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
