794f202760c6758193858049247d9fe212331f2f
Jacob Rein <>
2022-08-24 12:26:54 -0400

    Updating version

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -1,5 +1,5 @@
 object AppInfo {
-    const val otakuVersionName = "30.0.1"
+    const val otakuVersionName = "30.0.2"
     const val compileVersion = 33
     const val minimumSdk = 23
     const val targetSdk = 33
