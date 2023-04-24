a08a49d2c9f017b6a072ada514fedba340a056a6
Jacob Rein <jakerein@gmail.com>
2023-04-24 08:59:08 -0400

    Library updates!

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-serialization:${libs.versions.kotlin.get()}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${libs.versions.navVersion.get()}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0-alpha09")
     }
 }
 
