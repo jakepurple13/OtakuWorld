148bd58e5199d84039244e2126be98e49faf4a42
Jacob Rein <jakerein@gmail.com>
2023-09-01 09:58:01 -0400

    updating libs

--- sharedutils/build.gradle.kts
+++ sharedutils/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 dependencies {
     implementation(libs.material)
     testImplementation(TestDeps.junit)
-    testImplementation("com.jakewharton.picnic:picnic:0.6.0")
+    testImplementation("com.jakewharton.picnic:picnic:0.7.0")
     testImplementation("com.lordcodes.turtle:turtle:0.8.0")
     androidTestImplementation(TestDeps.androidJunit)
     androidTestImplementation(TestDeps.androidEspresso)
