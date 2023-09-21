27318b55e91d7b50bc4b440c626cea793afa3b80
Jacob Rein <jakerein@gmail.com>
2023-09-21 06:47:39 -0400

    updating libs

--- UIViews/build.gradle.kts
+++ UIViews/build.gradle.kts
@@ -121,7 +121,7 @@ dependencies {
 }
 
 protobuf {
-    protoc { artifact = "com.google.protobuf:protoc:3.24.2" }
+    protoc { artifact = "com.google.protobuf:protoc:3.24.3" }
     plugins {
         id("javalite") { artifact = libs.protobufJava.get().toString() }
         id("kotlinlite") { artifact = libs.protobufKotlin.get().toString() }
