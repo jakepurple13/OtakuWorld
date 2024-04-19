bdfdb5439716ebff348f10baa2f999648b249126
jacobrein <jacob.rein@gabb.com>
2024-04-19 10:52:16 -0600

    - testing something

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,21 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version ("3.16.2")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(
