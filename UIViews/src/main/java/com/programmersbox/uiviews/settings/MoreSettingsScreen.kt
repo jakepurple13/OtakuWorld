814d39502fc6749c189728ec83f8d2c928bbfc05
jacobrein <jacob.rein@gabb.com>
2024-04-24 12:06:25 -0600

    - cleanup

--- UIViews/src/main/java/com/programmersbox/uiviews/settings/MoreSettingsScreen.kt
+++ UIViews/src/main/java/com/programmersbox/uiviews/settings/MoreSettingsScreen.kt
@@ -1,7 +1,5 @@
 package com.programmersbox.uiviews.settings
 
-import android.content.Context
-import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
@@ -14,40 +12,22 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.util.fastMaxBy
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dokar.sonner.ToastType
 import com.dokar.sonner.rememberToasterState
-import com.programmersbox.favoritesdatabase.DbModel
 import com.programmersbox.favoritesdatabase.ItemDao
-import com.programmersbox.gsonutils.fromJson
-import com.programmersbox.gsonutils.toJson
-import com.programmersbox.sharedutils.FirebaseDb
 import com.programmersbox.uiviews.R
 import com.programmersbox.uiviews.utils.CategorySetting
 import com.programmersbox.uiviews.utils.LocalItemDao
 import com.programmersbox.uiviews.utils.PreferenceSetting
 import com.programmersbox.uiviews.utils.ToasterSetup
 import com.programmersbox.uiviews.utils.ToasterUtils
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.BufferedReader
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStreamReader
 import kotlin.time.Duration
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -127,76 +107,4 @@ fun MoreSettingsScreen(
     }
 
     ToasterSetup(toaster = toaster)
-}
-
-class MoreSettingsViewModel(
-    private val dao: ItemDao,
-) : ViewModel() {
-
-    var importExportListStatus: ImportExportListStatus by mutableStateOf(ImportExportListStatus.Idle)
-
-    fun exportFavorites() = listOf(
-        dao.getAllFavoritesSync(),
-        FirebaseDb.getAllShows().requireNoNulls()
-    )
-        .flatten()
-        .groupBy(DbModel::url)
-        .map { it.value.fastMaxBy(DbModel::numChapters)!! }
-
-    fun importFavorites(document: Uri, context: Context) {
-        viewModelScope.launch {
-            importExportListStatus = ImportExportListStatus.Loading
-            runCatching {
-                context.contentResolver.openInputStream(document)
-                    ?.use { inputStream -> BufferedReader(InputStreamReader(inputStream)).readText() }
-                    .fromJson<List<DbModel>>()
-            }
-                .onSuccess { list ->
-                    list?.forEach {
-                        dao.insertFavorite(it)
-                        FirebaseDb.insertShowFlow(it)
-                    }
-                    importExportListStatus = ImportExportListStatus.Success
-                }
-                .onFailure {
-                    it.printStackTrace()
-                    importExportListStatus = ImportExportListStatus.Error(it)
-                }
-        }
-    }
-
-    fun writeToFile(document: Uri, context: Context) {
-        importExportListStatus = ImportExportListStatus.Loading
-        runCatching {
-            viewModelScope.launch {
-                try {
-                    val exportFavorites = withContext(Dispatchers.IO) { exportFavorites() }
-                    context.contentResolver.openFileDescriptor(document, "w")?.use {
-                        FileOutputStream(it.fileDescriptor).use { f ->
-                            f.write(exportFavorites.toJson().toByteArray())
-                        }
-                    }
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-        }
-            .onSuccess {
-                println("Written!")
-                importExportListStatus = ImportExportListStatus.Success
-            }
-            .onFailure {
-                it.printStackTrace()
-                importExportListStatus = ImportExportListStatus.Error(it)
-            }
-    }
-}
-
-sealed class ImportExportListStatus {
-    data object Idle : ImportExportListStatus()
-    data object Loading : ImportExportListStatus()
-    class Error(val throwable: Throwable) : ImportExportListStatus()
-    data object Success : ImportExportListStatus()
 }
