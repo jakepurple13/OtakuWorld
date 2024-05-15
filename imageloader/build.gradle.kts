1c20d6cd22abcbb649f2d315b1c14b66da20144e
jacobrein <jacob.rein@gabb.com>
2024-05-15 09:06:28 -0600

    - updating libs

--- imageloader/build.gradle.kts
+++ imageloader/build.gradle.kts
@@ -15,7 +15,7 @@ otakuDependencies {
 
 compose {
     //kotlinCompilerPlugin.set("org.jetbrains.kotlin.plugin.compose:1.6.10-beta03"/*"androidx.compose.compiler:compiler:${libs.versions.jetpackCompiler.get()}"*/)
-    kotlinCompilerPlugin = "org.jetbrains.kotlin:kotlin-compose-compiler-plugin-embeddable:2.0.0-RC2"
+    kotlinCompilerPlugin = "org.jetbrains.kotlin:kotlin-compose-compiler-plugin-embeddable:${libs.versions.kotlin.get()}"
 }
 
 android {
