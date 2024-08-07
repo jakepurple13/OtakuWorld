77363fee97d6382db92ad24a1a560480b4aa5a46
jacobrein <jacob.rein@gabb.com>
2024-08-07 12:32:06 -0600

    - Update dependencies
    Update Compose to 1.7.0-beta07, Room to 2.7.0-alpha06, Navigation
     to 2.8.0-beta07, Paging to 3.3.2, and WorkManager to 2.9.1.
    Also update compile and target SDK to 35.

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -1,6 +1,6 @@
 object AppInfo {
     const val otakuVersionName = "31.0.2"
-    const val compileVersion = 34
+    const val compileVersion = 35
     const val minimumSdk = 28
-    const val targetSdk = 34
+    const val targetSdk = 35
 }
