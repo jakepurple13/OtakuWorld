6b219dff6df1a659c120fae599e7e585384dddb1
jacobrein <jacob.rein@gabb.com>
2024-08-30 08:42:02 -0600

    Refactor: Update launcher icon background color
    - Changed the launcher icon background color to use the dynamic color 'material_dynamic_primary95'.
    -
     Updated Ktor dependency to use 'JsoupPlugin' instead of 'JsoupFeature'.

--- Models/src/main/java/com/programmersbox/models/Utilities.kt
+++ Models/src/main/java/com/programmersbox/models/Utilities.kt
@@ -1,6 +1,6 @@
 package com.programmersbox.models
 
-import com.tfowl.ktor.client.features.JsoupPlugin
+import com.tfowl.ktor.client.plugins.JsoupPlugin
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.okhttp.OkHttp
