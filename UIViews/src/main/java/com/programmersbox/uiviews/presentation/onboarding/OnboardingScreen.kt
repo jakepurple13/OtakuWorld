2c33a9b22ac3bf9a4522627915d4c4b916337329
jacobrein <jacob.rein@gabb.com>
2025-04-15 10:42:13 -0600

    Refactor: Enhance Onboarding Screen with Theme Selection and Settings
    This commit refactors the Onboarding Screen to include theme selection and settings, improving the user's first-time experience.
    
    -   **Onboarding Screen Update:**
        -   Added a new page to the Onboarding Screen for theme selection.
        -   Increased the total number of pages in the pager from 2 to 3, with an additional page for full flavor builds.
        - Updated to have a theme screen so users can choose their theme
    - **Theme Content:**
    + - **Theme Content:**
         -   Added `ThemeContent` composable for theme selection.
         - Added a way to choose the theme mode (System, Day, Night).
         -   Added a setting for Amoled mode.
         -   Added a `FlowRow` to display theme color options.
        - Added a way to select the `themeColor`.

--- UIViews/src/main/java/com/programmersbox/uiviews/presentation/onboarding/OnboardingScreen.kt
+++ UIViews/src/main/java/com/programmersbox/uiviews/presentation/onboarding/OnboardingScreen.kt
@@ -2,12 +2,15 @@ package com.programmersbox.uiviews.presentation.onboarding
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,6 +24,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.SettingsBrightness
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -35,10 +40,13 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
@@ -48,13 +56,21 @@ import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
+import com.materialkolor.rememberDynamicColorScheme
 import com.programmersbox.sharedutils.AppLogo
 import com.programmersbox.uiviews.BuildConfig
 import com.programmersbox.uiviews.R
+import com.programmersbox.uiviews.SystemThemeMode
+import com.programmersbox.uiviews.ThemeColor
 import com.programmersbox.uiviews.datastore.DataStoreHandling
+import com.programmersbox.uiviews.datastore.SettingsHandling
 import com.programmersbox.uiviews.datastore.asState
 import com.programmersbox.uiviews.presentation.Screen
+import com.programmersbox.uiviews.presentation.components.ListSetting
 import com.programmersbox.uiviews.presentation.components.NormalOtakuScaffold
+import com.programmersbox.uiviews.presentation.components.SwitchSetting
+import com.programmersbox.uiviews.presentation.components.ThemeItem
+import com.programmersbox.uiviews.presentation.components.seedColor
 import com.programmersbox.uiviews.presentation.settings.viewmodels.AccountViewModel
 import com.programmersbox.uiviews.utils.HideSystemBarsWhileOnScreen
 import com.skydoves.landscapist.glide.GlideImage
@@ -90,7 +106,7 @@ fun OnboardingScreen(
 
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState {
-        2 + if (BuildConfig.FLAVOR == "full") 1 else 0
+        3 + if (BuildConfig.FLAVOR == "full") 1 else 0
     }
 
     NormalOtakuScaffold(
@@ -155,7 +171,6 @@ fun OnboardingScreen(
         //TODO: Add login screen
         // add account info screen
         // maybe even see if we can get the account created when
-        // maybe add a theme screen here
 
         HorizontalPager(
             state = pagerState,
@@ -169,8 +184,10 @@ fun OnboardingScreen(
                     appLogo = appLogo,
                 )
 
+                1 -> ThemeContent()
+
                 //TODO: Maybe make an enum for the flavors?
-                1 -> if (BuildConfig.FLAVOR == "full") {
+                2 -> if (BuildConfig.FLAVOR == "full") {
                     AccountContent()
                 } else {
                     FinishContent()
@@ -178,6 +195,103 @@ fun OnboardingScreen(
 
                 pagerState.pageCount -> FinishContent()
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+private fun ThemeContent(
+    handling: SettingsHandling = koinInject(),
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        ListItem(
+            headlineContent = {
+                Text(
+                    "Pick your theme!"
+                )
+            },
+        )
+
+        HorizontalDivider()
+
+        var themeSetting by handling.rememberSystemThemeMode()
+
+        val themeText by remember {
+            derivedStateOf {
+                when (themeSetting) {
+                    SystemThemeMode.FollowSystem -> "System"
+                    SystemThemeMode.Day -> "Light"
+                    SystemThemeMode.Night -> "Dark"
+                    else -> "None"
+                }
+            }
+        }
+
+        ListSetting(
+            settingTitle = { Text(stringResource(R.string.theme_choice_title)) },
+            dialogIcon = { Icon(Icons.Default.SettingsBrightness, null) },
+            settingIcon = { Icon(Icons.Default.SettingsBrightness, null, modifier = Modifier.fillMaxSize()) },
+            dialogTitle = { Text(stringResource(R.string.choose_a_theme)) },
+            summaryValue = { Text(themeText) },
+            confirmText = { TextButton(onClick = { it.value = false }) { Text(stringResource(R.string.cancel)) } },
+            value = themeSetting,
+            options = listOf(SystemThemeMode.FollowSystem, SystemThemeMode.Day, SystemThemeMode.Night),
+            updateValue = { it, d ->
+                d.value = false
+                themeSetting = it
+            }
+        )
+
+        var isAmoledMode by handling.rememberIsAmoledMode()
+
+        AnimatedVisibility(
+            visible = themeSetting != SystemThemeMode.Day
+        ) {
+            SwitchSetting(
+                settingTitle = { Text(stringResource(R.string.amoled_mode)) },
+                settingIcon = { Icon(Icons.Default.DarkMode, null, modifier = Modifier.fillMaxSize()) },
+                value = isAmoledMode,
+                updateValue = { isAmoledMode = it }
+            )
+        }
+
+        var themeColor by handling.rememberThemeColor()
+
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterHorizontally),
+            verticalArrangement = Arrangement.spacedBy(2.dp),
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            ThemeColor.entries
+                //TODO: For later
+                .filter { it != ThemeColor.Custom && it != ThemeColor.UNRECOGNIZED }
+                .forEach {
+                    ThemeItem(
+                        themeColor = it,
+                        onClick = { themeColor = it },
+                        selected = it == themeColor,
+                        colorScheme = if (it == ThemeColor.Dynamic)
+                            MaterialTheme.colorScheme
+                        else
+                            rememberDynamicColorScheme(
+                                it.seedColor,
+                                isDark = when (themeSetting) {
+                                    SystemThemeMode.FollowSystem -> isSystemInDarkTheme()
+                                    SystemThemeMode.Day -> false
+                                    SystemThemeMode.Night -> true
+                                    else -> isSystemInDarkTheme()
+                                },
+                                isAmoled = isAmoledMode
+                            )
+                    )
+                }
         }
     }
 }
