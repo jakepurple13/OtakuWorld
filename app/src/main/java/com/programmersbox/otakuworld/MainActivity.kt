358fc3f65e912988cf31084aab28de5bc238ae1a
jacobrein <jacob.rein@gabb.com>
2025-09-16 09:49:31 -0600

    Refactor: Update TODO comments in MainActivity
    This commit updates the TODO comments in `MainActivity.kt`.
    The comments now include ideas for additional content providers (notifications, incognito) and a reminder to create a README for the app.
    Removed some redundant questions and rephrased existing points for clarity.
    
    - **`app/src/main/java/com/programmersbox/otakuworld/MainActivity.kt`**:
        - Modified existing TODO comments to refine ideas for app functionality:
            - Added "(Maybe include more content providers for notifications, incognito, etc)?"
            - Removed "Maybe display to all of the sources for each app?"
            - Reworded "I'd need to modify the content providers to allow for everything that's allowed in the server"
            - Removed "Maybe it'd also be able view everything? Maybe clicking on items open it in that app?"
            - Removed "Probably also want to show some of the app info? Version code and name?"
            - Added "Create a readme for this app"

--- app/src/main/java/com/programmersbox/otakuworld/MainActivity.kt
+++ app/src/main/java/com/programmersbox/otakuworld/MainActivity.kt
@@ -52,16 +52,14 @@ class MainActivity : AppCompatActivity() {
 
         //TODO: MAYBE this app (or a new one) becomes the syncing with a custom server
         // It'd use Account Manager
-        // Maybe display to all of the sources for each app?
         // It'd have SyncAdapters for every one of the content providers
-        // I'd need to modify the content providers to allow for everything that's allowed in the server
+        // (Maybe include more content providers for notifications, incognito, etc)?
         // Login would need to be setup to communicate with the server,
         // making this app very modifiable and won't have any firebase stuff apart of it
         // I'd also need to make sure to notify whenever the databases change.
-        // Maybe it'd also be able view everything? Maybe clicking on items open it in that app?
-        // Probably also want to show some of the app info? Version code and name?
         // Maybe there's even reverse support? The apps try and detect if OtakuWorld is installed and logged in?
         // Maybe this app even handles firebase? And the OtakuWorld apps are purely local?
+        // Create a readme for this app
 
         setContent {
             val darkTheme = isSystemInDarkTheme()
