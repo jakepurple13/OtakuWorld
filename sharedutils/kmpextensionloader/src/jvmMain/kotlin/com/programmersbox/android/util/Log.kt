bc800e4ee94cde15ab16b835f01e3d0f4bd08bae
jacobrein <jacob.rein@gabb.com>
2026-05-01 09:25:12 -0600

    fix: add Log.w(tag,Throwable) overload and consolidate stacktrace output streams

--- sharedutils/kmpextensionloader/src/jvmMain/kotlin/com/programmersbox/android/util/Log.kt
+++ sharedutils/kmpextensionloader/src/jvmMain/kotlin/com/programmersbox/android/util/Log.kt
@@ -8,15 +8,16 @@ object Log {
     const val ERROR = 6
 
     @JvmStatic fun v(tag: String, msg: String): Int { println("V/$tag: $msg"); return 0 }
-    @JvmStatic fun v(tag: String, msg: String, tr: Throwable): Int { println("V/$tag: $msg"); tr.printStackTrace(); return 0 }
+    @JvmStatic fun v(tag: String, msg: String, tr: Throwable): Int { println("V/$tag: $msg\n${tr.stackTraceToString()}"); return 0 }
     @JvmStatic fun d(tag: String, msg: String): Int { println("D/$tag: $msg"); return 0 }
-    @JvmStatic fun d(tag: String, msg: String, tr: Throwable): Int { println("D/$tag: $msg"); tr.printStackTrace(); return 0 }
+    @JvmStatic fun d(tag: String, msg: String, tr: Throwable): Int { println("D/$tag: $msg\n${tr.stackTraceToString()}"); return 0 }
     @JvmStatic fun i(tag: String, msg: String): Int { println("I/$tag: $msg"); return 0 }
-    @JvmStatic fun i(tag: String, msg: String, tr: Throwable): Int { println("I/$tag: $msg"); tr.printStackTrace(); return 0 }
+    @JvmStatic fun i(tag: String, msg: String, tr: Throwable): Int { println("I/$tag: $msg\n${tr.stackTraceToString()}"); return 0 }
     @JvmStatic fun w(tag: String, msg: String): Int { println("W/$tag: $msg"); return 0 }
-    @JvmStatic fun w(tag: String, msg: String, tr: Throwable): Int { println("W/$tag: $msg"); tr.printStackTrace(); return 0 }
+    @JvmStatic fun w(tag: String, msg: String, tr: Throwable): Int { println("W/$tag: $msg\n${tr.stackTraceToString()}"); return 0 }
+    @JvmStatic fun w(tag: String, tr: Throwable): Int { println("W/$tag: ${tr.message}\n${tr.stackTraceToString()}"); return 0 }
     @JvmStatic fun e(tag: String, msg: String): Int { System.err.println("E/$tag: $msg"); return 0 }
-    @JvmStatic fun e(tag: String, msg: String, tr: Throwable): Int { System.err.println("E/$tag: $msg"); tr.printStackTrace(); return 0 }
+    @JvmStatic fun e(tag: String, msg: String, tr: Throwable): Int { System.err.println("E/$tag: $msg\n${tr.stackTraceToString()}"); return 0 }
     @JvmStatic fun wtf(tag: String, msg: String): Int { System.err.println("WTF/$tag: $msg"); return 0 }
-    @JvmStatic fun wtf(tag: String, msg: String, tr: Throwable): Int { System.err.println("WTF/$tag: $msg"); tr.printStackTrace(); return 0 }
+    @JvmStatic fun wtf(tag: String, msg: String, tr: Throwable): Int { System.err.println("WTF/$tag: $msg\n${tr.stackTraceToString()}"); return 0 }
 }
