8bf3a591596eacd325f56b4cb01977be67e7024a
jacobrein <jacob.rein@gabb.com>
2026-05-05 07:34:26 -0600

    build: update Compose Multiplatform and navigation dependencies
    - **Dependencies**:
        - Update `compose-multiplatform` from `1.11.0-beta03` to `1.11.0-rc01`.
        - Bump `filekitCore` to `0.14.0`.
        - Update `cmp-nav3` to `1.1.1` and `cmp-navevent` to `1.1.0-rc01`.
    - **Desktop**:
        - Add a TODO in `GenericInfo.jvm.kt` regarding the implementation of an "Open via webview" screen for the JVM target to improve chapter reading accessibility.

--- kmpuiviews/src/jvmMain/kotlin/com/programmersbox/kmpuiviews/GenericInfo.jvm.kt
+++ kmpuiviews/src/jvmMain/kotlin/com/programmersbox/kmpuiviews/GenericInfo.jvm.kt
@@ -1,3 +1,5 @@
 package com.programmersbox.kmpuiviews
 
 actual interface PlatformGenericInfo : KmpGenericInfo
+//TODO: Add a "Open via webview" screen for desktop so that the user can open the chapter in a webview
+// to read the chapter easier
