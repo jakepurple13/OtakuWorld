832829b32713aea307616c09dec79725cac2a69a
jacobrein <jacob.rein@gabb.com>
2026-07-09 12:16:43 -0600

    feat(backup): selective + peek Zipper API (common expect + Android actual)

--- kmpuiviews/src/commonMain/kotlin/com/programmersbox/kmpuiviews/utils/Zipper.kt
+++ kmpuiviews/src/commonMain/kotlin/com/programmersbox/kmpuiviews/utils/Zipper.kt
@@ -1,8 +1,25 @@
 package com.programmersbox.kmpuiviews.utils
 
+import com.programmersbox.sharedcomponents.backup.BackupDataSummary
+import com.programmersbox.sharedcomponents.backup.BackupUiInfo
+import com.programmersbox.sharedcomponents.backup.ItemResult
 import io.github.vinceglb.filekit.PlatformFile
 
 expect class Zipper {
-    suspend fun zipFile(platformFile: PlatformFile)
-    suspend fun readZip(platformFile: PlatformFile)
+    suspend fun zipFile(
+        platformFile: PlatformFile,
+        selectedKeys: Set<String>,
+        onItemComplete: suspend (ItemResult) -> Unit,
+    ): List<ItemResult>
+
+    suspend fun readZip(
+        platformFile: PlatformFile,
+        selectedKeys: Set<String>,
+        onItemComplete: suspend (ItemResult) -> Unit,
+    ): List<ItemResult>
+
+    suspend fun peekZip(
+        platformFile: PlatformFile,
+        uiInfos: List<BackupUiInfo>,
+    ): Map<String, BackupDataSummary>
 }
